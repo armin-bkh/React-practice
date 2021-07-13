19293a48905d243036839f526eb80ffd5e7fddc8
armin bakhshi <arminbkh0921@yahoo.com>
2021-07-13 15:58:43 +0430

    Routing Practice

--- src/Routing.js
+++ src/Routing.js
@@ -1,71 +1,93 @@
-import React from 'react';
-import { ReactDOM } from 'react-dom';
-import {NavLink, Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import  Homepage  from './Routaddress1';
-import  Contact  from './Routaddress2';
-import About from './Routaddress3';
-import NotFound from './404';
-import './Routing.scss';
+import React from "react";
+import { ReactDOM } from "react-dom";
+import {
+  NavLink,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import Homepage from "./Routaddress1";
+import Contact from "./Routaddress2";
+import About from "./Routaddress3";
+import NotFound from "./404";
+import "./Routing.scss";
 
-
-const Users = () =>{
-    return (
-        <h1>Users</h1>
-    ) 
-}
-const UserID = ({match, history}) =>{
-        console.log(match.params.ID);
-   const GoHandler = () =>{
-        history.push('/users');
-    }
+const Users = () => {
+  return (
+    <React.Fragment>
+      <h1>Users</h1>
+      <ul>
+        <li>
+          <Link to="/Users/Amir">User-1</Link>
+        </li>
+        <li>
+          <Link to="/Users/Reza">User-2</Link>
+        </li>
+      </ul>
+    </React.Fragment>
+  );
+};
+const UserID = ({ match, history }) => {
+  const ID = match.params.ID;
+  if (ID === "Amir" || ID === "Reza") {
     return (
         <React.Fragment>
-            <h6>User {match.params.ID}</h6>
-            <button type="button" onClick={GoHandler}>GO</button>
+            <h1>Welcome {ID}</h1>
+            <button onClick={()=> history.push('/Users')}>Go</button>
         </React.Fragment>
-        ) 
-}
-
-function Routing(){
-
-    return (
-        <Router>
-        <ul className="menu">
+        );
+  } else {
+    return <NotFound />;
+  }
+};
+const Routing = () => {
+  return (
+    <Router>
+      <ul className="menu">
+        <li>
+          <NavLink activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/Contact">
+            Contact
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/About">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/Users">
+            Users
+          </NavLink>
+          <ul>
             <li>
-                <Link to="/">HomePage</Link>
+              <NavLink activeClassName="active" to="/Users/Amir">
+                User-1
+              </NavLink>
             </li>
             <li>
-                <Link to="/contact">Contact</Link>
+              <NavLink activeClassName="active" to="/Users/Reza">
+                User-2
+              </NavLink>
             </li>
-            <li>
-                <Link to="/About">About</Link>
-            </li>
-            <li>
-                <Link to="/users">Users</Link>
-                <ul>
-                <li>
-                <Link to="/users/1">User 1</Link>
-            </li>
-            <li>
-                <Link to="/users/2">User 2</Link>
-            </li>         
-                </ul>
-            </li>
-               
-        </ul>
-        
-        <Switch>
+          </ul>
+        </li>
+      </ul>
+      <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/contact" component={Contact} />
-        <Route path="/About" component={About} />
-        <Route exact path="/Users" component={Users} />
-        <Route path="/Users/:ID" component={UserID} />
+        <Route exact path="/Contact" component={Contact} />
+        <Route exact path="/About" component={About} />
+        <Route exact exact path="/Users" component={Users} />
+        <Route exact path="/Users/:ID" component={UserID} />
         <Route component={NotFound} />
-        </Switch>
-        
-        </Router>
-    )
-
-}
+      </Switch>
+    </Router>
+  );
+};
 
 export default Routing;
